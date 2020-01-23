a5cc95c5408c7d4a8611ea43c1b9ac385291a351
hassankhamis97 <hassankhamis97@hotmail.com>
2020-01-23 04:24:49 +0200

    test2

--- src/javaproject/Main.java
+++ src/javaproject/Main.java
@@ -5,6 +5,12 @@
  */
 package javaproject;
 
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -16,9 +22,29 @@ import javafx.stage.Stage;
  * @author hassan
  */
 public class Main extends Application {
-        
+        Connection con;
+        ResultSet rs;
+        PreparedStatement stmt;
         @Override
         public void start(Stage stage) throws Exception {
+                ///// Check for The DB Jar File Included
+                ClassLoader cl = ClassLoader.getSystemClassLoader();
+                URL[] urls = ((URLClassLoader) cl).getURLs();
+                String urlStr = "";
+                for (int i=0; i < urls.length; i++) {
+                   urlStr += urls[i].getFile() + "\n";
+                }
+                System.out.println("Classpath:\n" + urlStr);
+                //// end of Check for The DB Jar File Included
+                Class.forName("com.mysql.jdbc.Driver"); // stablish the connection
+                con = DriverManager.getConnection("jdbc:mysql://sql7.freemysqlhosting.net:3306/sql7320143", "sql7320143", "LS4CgdqGST");
+                
+                
+                //con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sql7320143", "root", "root"); // local
+                
+                stmt = con.prepareStatement("SELECT ID,Name,Email,Password FROM Player");
+                
+                rs = stmt.executeQuery();
                 Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
                 
                 Scene scene = new Scene(root);
